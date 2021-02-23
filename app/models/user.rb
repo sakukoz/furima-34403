40cee4190c0913c6c9ae7074da47acc46abdb576
sakuraka <sakuraka@users.noreply.github.com>
2021-02-23 16:59:44 +0900

    first_name,last_nameバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,11 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :birthday, presence: true
 
+  with_options presence: true do
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+
+  end
 end
