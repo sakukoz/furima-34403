bf031a1cf86114070689b1effb68071c1cd03e85
sakuraka <sakuraka@users.noreply.github.com>
2021-02-24 18:03:07 +0900

    商品の情報をテーブルに保存できるよう実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -9,7 +10,7 @@ class Item < ApplicationRecord
   belongs_to :region
 
 
-  validates :item_name, :description,:category_id,:condition_id, :delivery_fee_id, :region_id, :delivery_days_id, :price, :user, presence: true
-  validates :genre_id, numericality: { other_than: 1 } 
+  validates :item_name, :image, :description,:category_id,:condition_id, :delivery_fee_id, :region_id, :delivery_days_id, :price, :user, presence: true
+  validates :category_id, :condition_id, :delivery_days_id, :delivery_fee_id, :region_id, numericality: { other_than: 1 } 
 
 end
