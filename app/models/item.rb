b0ec0727f58214fa6a730bcd1dda812ddc983a03
sakuraka <sakuraka@users.noreply.github.com>
2021-02-24 14:48:00 +0900

    商品出品ページを表示
    ルーティング
    Form_withの編集追記
    商品詳細カラム作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_days
+  belongs_to :delivery_fee
+  belongs_to :region
+
+
+  validates :item_name, :description,:category_id,:condition_id, :delivery_fee_id, :region_id, :delivery_days_id, :price, :user, presence: true
+  validates :genre_id, numericality: { other_than: 1 } 
 
 end
