97d725b43ec4ec3353a32038253c7fea09b139ab
sakuraka <sakuraka@users.noreply.github.com>
2021-03-01 18:43:41 +0900

    フォームの情報を保存できるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,16 +2,22 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id]) 
-
   end
 
   def create
+    @item = Item.find(params[:item_id]) 
     @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end 
   end
 
   private
   def order_params
-    params.require(:order_address).permit(:city, :order, :postcode, :region_id, :city, :street, :building, :phone).merge(user_id: current_user.id, item: params[:item_id])
+    params.require(:order_address).permit(:city, :order, :postcode, :region_id, :city, :street, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
  
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,19 +1,19 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :item, :user, :city, :order, :postcode, :region_id, :city, :street, :building, :phone
+  attr_accessor :item_id, :user_id, :city, :postcode, :region_id, :city, :street, :building, :phone
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :order
-    validates :postcode
-    validates :region_id, numericality: { other_than: 1 }
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :street
-    validates :phone
+    validates :phone, numericality: { only_integer: true , },length: { is: 11 }
   end
+    validates :region_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postcode: postcode, region: region, city: city, street: street, building: building, phone: phone, order_id: order_id)
+    Address.create(postcode: postcode, region_id: region_id, city: city, street: street, building: building, phone: phone, order_id: order.id)
   end
 end
